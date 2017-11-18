46c13aefa296377e8ca29454f7db5dfa504334b5
John <vim2meta@tutanota.com>
2017-11-18 09:55:06 -0500

    even better

--- src/commands/items/Buy.js
+++ src/commands/items/Buy.js
@@ -37,7 +37,7 @@ class Buy extends patron.Command {
       let description = '';
 
       for (let i = 0; i < result.rows.length; i++) {
-        description += '`$buy "' + StringUtil.capitializeWords(result.rows[i].name) + '" 1`: `' + USD(result.rows[i].price) + '` - ' + result.rows[i].description + '\n';
+        description += USD(result.rows[i].price) + ': `$buy "' + StringUtil.capitializeWords(result.rows[i].name) + '"` - ' + result.rows[i].description + '\n';
       }
 
       return sender.send(description, { title: 'Crates' });
