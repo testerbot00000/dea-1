d7856141ac9f6a3818ec4b6348a88f2ec778e08e
Assley <ashwafaw@gmail.com>
2017-11-24 16:31:03 -0500

    Create ResetCooldowns.js

---
+++ src/commands/administration/ResetCooldowns.js
@@ -0,0 +1,34 @@
+const patron = require('patron.js');
+const StringUtil = require('../../utility/StringUtil.js');
+
+class ResetCooldowns extends patron.Command {
+  constructor() {
+    super({
+      names: ['resetcooldowns', 'deletecooldowns', 'resetcds'],
+      groupName: 'administration',
+      description: 'Reset any member\'s cooldown on a command',
+      args: [
+        new patron.Argument({
+          name: 'member',
+          key: 'member',
+          type: 'member',
+          example: 'Big Willy#1234',
+          defaultValue: patron.ArgumentDefault.Member,
+          remainder: true
+        })
+      ]
+    });
+  }
+
+  run(msg, args, sender) {
+    const commands = msg.client.registry.commands.filter((command) => command.hasCooldown === true);
+
+    for (let i = 0; i < commands.length; i++) {
+      delete commands[i].cooldowns[args.member.id + '-' + msg.guild.id];
+    }
+
+    return sender.reply('You have successfully reset all of ' + (args.member.id === msg.author.id ? 'your' : StringUtil.boldify(args.member.user.tag) + '\'s') + ' cooldowns.');
+  }
+}
+
+module.exports = new ResetCooldowns();
