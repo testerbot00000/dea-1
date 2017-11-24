408c627f66d4db982f39618282c72538d249f8de
Assley <ashwafaw@gmail.com>
2017-11-23 21:35:19 -0500

    Cooldowns cmd

---
+++ src/commands/general/Cooldowns.js
@@ -0,0 +1,50 @@
+const patron = require('patron.js');
+const StringUtil = require('../../utility/StringUtil.js');
+const NumberUtil = require('../../utility/NumberUtil.js');
+
+class Cooldowns extends patron.Command {
+  constructor() {
+    super({
+      names: ['cooldowns', 'cd'],
+      groupName: 'general',
+      description: 'View all command cooldowns of a member',
+      args: [
+        new patron.Argument({
+          name: 'member',
+          key: 'member',
+          type: 'member',
+          example: 'b1nzy#1337',
+          defaultValue: patron.ArgumentDefault.Member,
+          remainder: true
+        })
+      ]
+    });
+  }
+
+  run(msg, args, sender) {
+    const commands = msg.client.registry.commands.filter((command) => command.hasCooldown === true);
+    let cooldowns = '';
+
+    for (let i = 0; i < commands.length; i++) {
+      const cooldown = commands[i].cooldowns[args.member.id + '-' + msg.guild.id];
+
+      if (cooldown !== undefined) {
+        const remaining = cooldown - Date.now();
+
+        if (remaining > 0) {
+          const formattedCooldown = NumberUtil.msToTime(remaining);
+
+          cooldowns += StringUtil.upperFirstChar(commands[i].names[0]) + ': ' + formattedCooldown.hours + ':' + formattedCooldown.minutes + ':' + formattedCooldown.seconds + '\n';
+        }
+      }
+    }
+
+    if (StringUtil.isNullOrWhiteSpace(cooldowns) === true) {
+      return sender.reply('All commands are ready to use.');
+    }
+
+    return sender.send(cooldowns, { title: 'All cooldowns for ' + args.member.user.tag });
+  }
+}
+
+module.exports = new Cooldowns();
